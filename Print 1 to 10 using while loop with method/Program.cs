6631cfe29e72f77fd8b3f3958ab4af6dd5a0426b
NET <NET@DESKTOP-NSMUMFG>
2022-10-22 11:15:10 +0600

    Add project files.

---
+++ Print 1 to 10 using while loop with method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_1_to_10_using_while_loop_with_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print 1 to 10");
+
+            Print();
+
+            Console.ReadKey();
+
+        }//Main
+
+
+        public static void Print()
+        {
+            int i = 1;
+            while(i <= 10)
+            {
+                Console.WriteLine($"{i} \t");
+                i++;
+            }
+        }
+    }//Class
+}//Namespace
